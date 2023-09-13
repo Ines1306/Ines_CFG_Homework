74b504b7bb4a2290ff7af1c1707d5f3470402b79
Ines1306 <82465315+Ines1306@users.noreply.github.com>
2023-09-13 21:33:25 +0100

    Update scriptOffer.js

--- CFG Homework#1/scriptOffer.js
+++ CFG Homework#1/scriptOffer.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function getDataFromHomePage () {
+document.addEventListener("DOMContentLoaded", function loadScript2() {
 
 // Return a random order number up to 1000
 const randomNumbers=[]
@@ -12,12 +12,14 @@ for (let i=0; i<1000; i++) {
 let randomOrder = randomNumbers[999] // we pick the last random order of the array
 console.log(randomOrder) // making sure the randomOrder is working
 
-if (randomOrder) {
-    document.querySelector("h6").innerHTML = `Great! Your order number is ${randomOrder}.
-    \n See you in 7 days!`
+if (randomOrder > 0) {
+    const randomOrderWorking = true // using boolean values
+    if (randomOrderWorking) {
+        document.querySelector("h6").innerHTML = (`Great! Your order number is ${randomOrder}.
+        \n See you in 7 days!`)
+    }
 }
 else {
     document.innerHTML = "Ups! Something went wrong with your order. Please visit our site at another time."
 }
-
 })
