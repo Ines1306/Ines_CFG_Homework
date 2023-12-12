e3a71c8d5fe3411edfc8835e957b646069d2d7d8
inesbveiga <inesbrancoveiga@gmail.com>
2023-12-12 23:12:30 +0000

    Adding topic1

---
+++ CFG Homework #4 - Software_Specialization/Topic1.py
@@ -0,0 +1,32 @@
+"""
+Question 1
+"""
+
+"""
+A tree has a root node, left and right nodes.
+"""
+
+"""
+Question 2
+"""
+
+"""
+A k-nary tree is a type of tree with nodes with k child nodes.
+"""
+
+"""
+Question 3
+"""
+
+"""
+The tree on the right is a perfect binary tree because its interior nodes have two child nodes and
+are all at the same level.
+"""
+
+"""
+Question 4
+"""
+
+"""
+This tree would be inefficient because the child nodes are not as far left as possible.
+"""
